5c096e4c0da2bad957633dcf1a8c4d805f42d546
Saching35 <62967794+Saching35@users.noreply.github.com>
2021-04-08 02:27:38 +0100

    Adding payload parameter and json-simple library

--- HintMateBackend/src/main/java/com/hintmate/springboot/controllers/FilterWordsController.java
+++ HintMateBackend/src/main/java/com/hintmate/springboot/controllers/FilterWordsController.java
@@ -1,16 +1,18 @@
 package com.hintmate.springboot.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilterWordsController {
 
     @RequestMapping("/fetchFilteredWords")
-    public List fetchFilteredWords() {
+    public List fetchFilteredWords(@RequestParam(value="payload")JSONObject payload) {
         List<String> filteredWords = new ArrayList<>();
         filteredWords.add("word1");
         filteredWords.add("2ndWord");
--- HintMateBackend/src/main/java/com/hintmate/springboot/controllers/HintsController.java
+++ HintMateBackend/src/main/java/com/hintmate/springboot/controllers/HintsController.java
@@ -1,6 +1,8 @@
 package com.hintmate.springboot.controllers;
 
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,7 +11,9 @@ import java.util.List;
 @RestController
 public class HintsController {
     @RequestMapping("/getMeaning")
-    public String getWordMeanings() {
-        return "This word means That";
+    public String getWordMeanings(@RequestParam(value="payload")String payload) {
+    	String meaning = "ABCDE";
+    	String res = payload + " Means " + meaning; 
+        return res;
     }
 }
